9be6cf1827dc1f09ec89498f4eb3e0ae83242405
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2025-02-05 12:35:28 +0100

    feat: initial bot

--- code/bot.py
+++ code/bot.py
@@ -1,12 +1,21 @@
-import os
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-load_dotenv()
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+# Load config directly
+config = dotenv_values(".env")
+BOT_TOKEN = config.get("TELEGRAM_TOKEN")
+
+# print(f"Loaded token: {BOT_TOKEN}")  # Debug print
+# print(f"Token type: {type(BOT_TOKEN)}")
 
 
-def main():
-    print(BOT_TOKEN)
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
-main()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
