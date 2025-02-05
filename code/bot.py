bf38e770dfb6ac9f1cfa36c33b9a08b1247f03cc
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2025-02-05 13:05:47 +0100

    feat: add needed libraries

--- code/bot.py
+++ code/bot.py
@@ -1,18 +1,35 @@
 from dotenv import dotenv_values
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+import json
+from pathlib import Path
+
+# States for conversations
+(
+    WAITING_FOR_NUMBER,
+    WAITING_FOR_NAME,
+    WAITING_FOR_EDIT_CHOICE,
+    WAITING_FOR_EDIT_VALUE,
+) = range(4)
+
 
 # Load config directly
 config = dotenv_values(".env")
 BOT_TOKEN = config.get("TELEGRAM_TOKEN")
 
-# print(f"Loaded token: {BOT_TOKEN}")  # Debug print
-# print(f"Token type: {type(BOT_TOKEN)}")
-
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
+async def addUser(update: Update, )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
