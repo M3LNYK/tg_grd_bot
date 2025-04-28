5e008680bdb08cef0550b5bc522a160463440c8c
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2025-04-28 12:10:37 +0200

    fix: change escape_markdown() to init text

--- code/bot.py
+++ code/bot.py
@@ -198,8 +198,9 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 # Helper function to escape MarkdownV2 characters
 def escape_markdown(text: str) -> str:
     """Helper function to escape telegram MarkdownV2 characters."""
+    text = str(text)
     escape_chars = r"_*[]()~`>#+-=|{}.!"
-    return "".join(f"\\{char}" if char in escape_chars else char for char in str(text))
+    return "".join(f"\\{char}" if char in escape_chars else char for char in text)
 
 
 # Helper function to format the student list and create keyboard
